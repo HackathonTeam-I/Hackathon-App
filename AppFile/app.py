67e8373a01f0cc8db546891123d61fd4f4d53b62
nagano-t785 <noftmk03@gmail.com>
2026-05-01 09:45:06 +0900

    /postsと/api/postsのルーティング追加＋アプリ起動設定追加（host/port/debug）

--- AppFile/app.py
+++ AppFile/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect,  render_template, session, flash, abort, url_for
+from flask import Flask, jsonify,request, redirect,  render_template, session, flash, abort, url_for
 from flask_wtf.csrf import CSRFProtect
 from datetime import timedelta
 import hashlib
@@ -6,8 +6,7 @@ import uuid
 import re
 import os
 
-from models import Post, Thread, Message
-
+from models import Post,Thread,Message
 
 # 定数定義　メール形式チェック用の正規表現とセッション有効期間（日数）を定義
 EMAIL_PATTERN = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
@@ -25,3 +24,30 @@ app.permanent_session_lifetime = timedelta(days=SESSION_DAYS)
 
 #悪意あるサイトから勝手にリクエストを送られる攻撃
 csrf = CSRFProtect(app)
+
+"""
+投稿機能
+"""
+#投稿内容を取得
+@app.route('/api/posts',methods=['GET'])
+def get_posts():
+    posts = Post.get_all_posts()
+    return jsonify(posts)
+
+# 投稿一覧表示
+@app.route('/posts',methods=['GET'])
+def show_posts():
+    posts = Post.get_all_posts()
+    if posts:
+        return render_template('post/posts.html',posts=posts)
+    else:
+        return render_template('post/posts.html',message='投稿内容がありません')
+
+
+
+
+"""
+プログラム実行
+"""
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
