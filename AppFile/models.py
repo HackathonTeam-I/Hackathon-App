fb6c207c36999ab84899cb53791ae0af029afa36
nagano-t785 <noftmk03@gmail.com>
2026-05-01 20:03:01 +0900

    models.py：create_posts()の追加

--- AppFile/models.py
+++ AppFile/models.py
@@ -52,3 +52,30 @@ class Post:
             abort(500)
         finally:
             db_pool.release(conn)
+
+    @classmethod
+    def create_posts(cls,user_id,category_id,found_date,found_place,description):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = """
+                INSERT INTO
+                posts (user_id,category_id,found_date,found_place,description) VALUES (%s,%s,%s,%s,%s)
+                """
+                cur.execute(sql,(user_id,category_id,found_date,found_place,description))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'サーバー接続上のエラーが発生しています：{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+
+    @classmethod
+    def update_posts(cls,id):
+        pass
+
+
+    @classmethod
+    def delete_posts(cls,id):
+        pass
