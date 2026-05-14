26c820d2eb2ad5bcc337a6d30317dbabcac40f73
nagano-t785 <noftmk03@gmail.com>
2026-05-14 21:10:46 +0900

    Messagesテーブルにthread_idカラムを追加

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -83,7 +83,7 @@ CREATE TABLE IF NOT EXISTS images (
 
 -- （７）DMリストテーブル
 CREATE TABLE IF NOT EXISTS threads (
-  id BIGINT UNSIGNED AUTO_INCREMENT,
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   user_id BIGINT UNSIGNED NOT NULL,
   created_at DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY(id),
@@ -95,13 +95,16 @@ CREATE TABLE IF NOT EXISTS messages (
   id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   post_id BIGINT UNSIGNED NOT NULL,
   sender_id BIGINT UNSIGNED NOT NULL,
+  thread_id BIGINT UNSIGNED NOT NULL,
   content TEXT NOT NULL,
   created_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY (id),
   KEY idx_messages_post (post_id),
   KEY idx_messages_sender (sender_id),
+  KEY idx_messages_thread(thread_id),
   CONSTRAINT fk_messages_post FOREIGN KEY (post_id) REFERENCES posts (id),
-  CONSTRAINT fk_messages_sender FOREIGN KEY (sender_id) REFERENCES users (id)
+  CONSTRAINT fk_messages_sender FOREIGN KEY (sender_id) REFERENCES users (id),
+  CONSTRAINT fk_messages_thread FOREIGN KEY(thread_id) REFERENCES threads(id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- （９）通知テーブル
