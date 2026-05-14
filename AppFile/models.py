18d4f4165fa658e81e55eb54ad3660fb470d8f0b
nagano-t785 <noftmk03@gmail.com>
2026-05-15 04:24:45 +0900

    Departmentクラスを追加

--- AppFile/models.py
+++ AppFile/models.py
@@ -5,6 +5,26 @@ from util.DB import DB
 # 初期起動時にコネクションプールを作成して、接続を確立
 db_pool = DB.init_db_pool()
 
+# Departmentクラス
+class Department:
+    @classmethod
+    def get_all_departments(cls):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = """
+                SELECT *
+                FROM departments
+                ORDER BY id ASC;
+                """
+                cur.execute(sql)
+                return cur.fetchall()
+        except pymysql.Error as e:
+            print(f'サーバー接続上のエラーが発生しています{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
 # Userクラス
 class User:
     @classmethod
@@ -24,6 +44,7 @@ class User:
             abort(500)
         finally:
             db_pool.release(conn)
+
 
 # Postクラス
 class Post:
